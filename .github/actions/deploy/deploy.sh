bc0e8ce07d1cb91f9274b7b1bf24be52bab8f4f4
Valentin Vesvalo <vesvalo@mail.ru>
2019-02-01 16:45:04 +0300

    fix

--- .github/actions/deploy/deploy.sh
+++ .github/actions/deploy/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+echo "123123123"
+
 kubectl config set-cluster k8s --insecure-skip-tls-verify=true --server=$K8S_API_URL &&
 kubectl config set-credentials ci --token=$K8S_CI_TOKEN &&
 kubectl config set-context ci --cluster=k8s --user=ci &&
 kubectl config use-context ci
 kubectl config get
+
